dba2cf8b184fac633d7e960ae264efcecc52ce1c
Kawsar Bin Siraj <38612699+kawsarBinSiraj@users.noreply.github.com>
2021-09-30 11:17:44 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+/**
+ * @Script js for (Template/Project Name)
+ *
+ * @project     - Project Name
+ * @author      - 
+ * @created_by  - 
+ * @created_at  - 
+ * @modified_by -
+ */
+
+
+/**
+ * ========================================================
+ * this function execute when window properly loaded
+ * ===========================================================
+ */
+
+$(window).on('load', function () {
+
+    // code should be execute here
+
+});
+
+
+
+/**
+ * ========================================================
+ * this function execute when DOM element ready 
+ * ===========================================================
+ */
+
+$(document).ready(function () {
+
+
+    // init 
+    // Masonry with filter
+    $(function () {
+        // masonry init
+        var masonry = new Masonry($('#masonry-grid-row').get()[0], {
+            itemSelector: ".grid-thumbnail"
+        });
+
+        $grid = $('#masonry-grid-row').isotope({
+            itemSelector: '.grid-thumbnail',
+            layoutMode: 'masonry',
+            masonry: {
+                gutter: 0
+            }
+        });
+
+        // filter activity
+        $('#filters').on('click', 'button', function () {
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({ filter: filterValue });
+            $('#filters button').removeClass('btn-primary').addClass('btn-info');
+            $(this).addClass('btn-primary').removeClass('btn-info');
+        });
+
+    });
+
+
+});
